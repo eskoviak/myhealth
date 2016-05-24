f1a899d26b7b04b5f58179dde9fb8e6bc8331001
Edmund L Skoviak <eskoviak@Edmund-L-Skoviaks-MacBook-Pro.local>
2016-05-23 21:10:54 -0400

    updated test_conn.py

--- python/test_conn.py
+++ python/test_conn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import MySQLdb
+import os
 
 db=MySQLdb.connect(
     host='104.196.12.164',
@@ -10,8 +11,10 @@ db=MySQLdb.connect(
 
 cur=db.cursor()
 
-cur.execute('SELECT * FROM Play')
+#cur.execute('SELECT * FROM Play')
+cur.execute('SELECT * FROM FOOD_DES WHERE MATCH(Long_Desc) AGAINST(os.argv[1])')
 
+print('NDB_No', 'Long Description')
 for row in cur.fetchall():
     print(row[0], row[1]) 
 
