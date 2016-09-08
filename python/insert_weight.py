39268514487c624ac17535ece4b8ad51467d8e41
eskoviak <eskoviak@gmail.com>
2016-09-08 17:00:58 +0000

    midday 9/9 Gulch

--- python/insert_weight.py
+++ python/insert_weight.py
@@ -2,6 +2,7 @@ import MySQLdb
 import sys
 import string
 import json
+import time
 
 db=MySQLdb.connect(
     host='104.196.12.164',
@@ -14,7 +15,13 @@ cur=db.cursor()
 cur.execute("""SELECT clientId FROM Clients WHERE lastName = %s""", ('Skoviak', ))
 
 clientId = cur.fetchone()[0]
+currTime = '2016-09-08 12:00:00'
 
-print(clientId)
-print(today())
+fp = open('weight.json', 'r')
+data = json.load(fp)
+
+cur.execute(str("INSERT INTO Weight VALUES (\'{0}\', \'{1}\', \"{2}\");").format(clientId, currTime, data))
+#print(str("INSERT INTO Weight VALUES (\'{0}\', \'{1}\', \"{2}\");").format(clientId, currTime, data))
+db.commit()
+fp.close()
 db.close()
