e91ef27cca0155073b075cd818d745492726cc1d
eskoviak <eskoviak@gmail.com>
2016-10-10 08:09:26 -0400

    EOD 10/10

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
 
+        weight = self.request.get('weight', '')
+        bodyFat = self.request.get('bodyFat', '')
+        leanBodyMass = self.request.get('leanBodyMass', '')
+        BMR = self.request.get('BMR', '')
         user = users.get_current_user()
         if user:
             url = users.create_logout_url(self.request.uri)
@@ -28,6 +32,10 @@ class MainPage(webapp2.RequestHandler):
             url_linktext = 'Login'
         
         template_values = {
+            'weight': weight,
+            'bodyFat': bodyFat,
+            'leanBodyMass': leanBodyMass,
+            'BMR': BMR,
             'user': user,
             'url_linktext': url_linktext,
         }
@@ -36,8 +44,34 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 # [END main_page]
 
+# [START weight]
+class Weight(webapp2.RequestHandler):
+
+    def post(self):
+        # The form calls the post method
+        # get the weight and bodyFat from the post
+        weight = float(self.request.get('weight', 0.0))
+        bodyFat = float(self.request.get('bodyFat', 0.0))
+
+        # Assume for now the body fat is #.## % format
+        leanBodyMass = weight * ( 1.0 - bodyFat/100)
+
+        # Assume for now the weight is in lbm
+        BMR = (leanBodyMass/2.204) * 21.6 + 370
+
+        query_params = {
+            'weight': str(weight),
+            'bodyFat': str(bodyFat),
+            'leanBodyMass': str(leanBodyMass),
+            'BMR': str(BMR)
+        }
+
+        self.redirect('/?'+urllib.urlencode(query_params))
+# [END weight]
+
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/weight', Weight),
 ], debug=True)
 # [END app]
