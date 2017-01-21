45688b26d9548125294f15bc1aad2c8d6582c870
eskoviak <eskoviak@gmail.com>
2017-01-20 20:58:35 -0500

    ATL MBP

--- python/healthCalc.py
+++ python/healthCalc.py
@@ -1,32 +1,58 @@
+# -*- coding: utf-8 -*-
 """
-  Module healthCalc
+  Module HealthCalc.  This module contains utility routines used for various health calculations.
 """
 import json
 
-def getCalories(inTuple, weight, time):
-  rise=inTuple[2]-inTuple[0]
-  run=60
-  slope=float(rise)/float(run)
-  y0=float(inTuple[0])-slope*125.0
-  return (slope*weight+y0)*(time/30)
+def get_calories(in_tuple, weight, time):
+    """Function get_calories.  Given input tuple of data from the actiity dictionary and the
+    specified weight and time, return the calculated calories.
 
-def getActivity(inQuery):
-  # load the dictionary
-  # return array of keys which match (case insensitive) the inQuery string
-  activities = []
-  fp = open('harvard.json', 'r')
-  harvard = json.load(fp)
-  for key in harvard.keys():
-      if inQuery.upper() in key.upper():
-          activities.append(key)
-  return activities
+    Args:
+        in_tuple (list):  a tuple in the form of (N1, N2, N3), where N1 is the calories expended
+        at 135 lbm, N2 @ 155 lbm and N3 @ 175 lbm, per 30 minutes for the given activity.abs
+        weight (float): the weight, in lbm.
+        time (flost): the time, in minutes
 
-def getActivityData(activityKey):
-  # load the dictionary
-  fp = open('harvard.json', 'r')
-  harvard = json.load(fp)
-  return harvard[activityKey]
+    Returns:
+        float:  the caluculated calories
+    """
+    rise = float(in_tuple[2]) - float(in_tuple[0])
+    run = 60
+    slope = float(rise)/float(run)
+    y_0 = float(in_tuple[0])-slope*125.0
+    return (slope*weight+y_0)*(time/30)
 
-def toKg(inweight):
-  return inweight/2.204
+def get_activity(query):
+    """Function getActivity.  Load the dictionary and search for item descriptions that match
+    the query.
 
+    Args:
+        query (str): the query phrase to search for
+
+    Returns:
+        list: A list of keys in the dictionary which whose description matches the inQuery.
+
+    """
+    activities = []
+    for key in json.load(open('activities.json', 'r')).keys():
+        if query.upper() in key.upper():
+            activities.append(key)
+    return activities
+
+def get_activity_data(activity_key):
+    """Function get_activity_data.  Returns the tuple associated with the key.
+
+    Args:
+        activity_key (str):  The key for the desired activity_key
+
+    Returns:
+        a tuple of the form (N1, N2, N3), where N1 is the calories for 135 lbm, N2 @ 155 lbm
+        and N3 @ 175 lbm, over a 30 minute period.
+
+    """
+    harvard = json.load(open('harvard.json', 'r'))
+    return harvard[activity_key]
+
+#def toKg(inweight):
+#  return inweight/2.204
--- python/test_main.py
+++ python/test_main.py
@@ -3,29 +3,35 @@
   program used to test other routines
 """
 
-import healthCalc
+import HealthCalc
 
-weight = float(input('Enter Weight (lbm): '))
-time = float(input('Enter time (min): '))
-inActivity = raw_input('Enter search phrase: ')
-activities = healthCalc.getActivity(inActivity)
-numItems = len(activities)
-if numItems == 0:
-  print('No item Not Found')
-  exit()
-if numItems == 1:
-  print('Process the item ' + activities[0])
-  print(healthCalc.getCalories(healthCalc.getActivityData(activities[0]), weight, time))
-  exit()
-if numItems > 0:
-  counter = 1
-  for a in activities:
-    print(str(counter) + ': ' + a)
-    counter += 1
-  choice = int(input('Enter number of your choice: '))
-  if choice > 0 and choice <= counter:
-    print('Proces the item: ' + activities[choice - 1])
-    print(healthCalc.getCalories(healthCalc.getActivityData(activities[choice - 1]), weight, time))
-  else:
-    print('Bad choice')
-  exit()
+# lambda functions:
+activity = lambda key: HealthCalc.get_activity(key)
+calories = lambda activity, weight, time: HealthCalc.get_calories(activity, weight, time)
+
+if __name__ == "__main__":
+    weight = float(input('Enter Weight (lbm): '))
+    time = float(input('Enter time (min): '))
+    query = raw_input('Enter search phrase: ')
+    activities = HealthCalc.get_activity(query)
+    numItems = len(activities)
+    if numItems == 0:
+        print 'No item Not Found'
+        exit()
+    if numItems == 1:
+        print 'Process the item ' + activities[0]
+        print()
+        exit()
+    if numItems > 0:
+        counter = 1
+        for a in activities:
+            print str(counter) + ': ' + a
+            counter += 1
+        choice = int(input('Enter number of your choice: '))
+        if choice > 0 and choice <= counter:
+            print 'Proces the item: ' + activities[choice - 1]
+            print 'For ' + str(time) + ' min @ ' + str(weight) + ' lbm'
+            print '-----------------------------------------'
+            print str(calories(activities[choice - 1], weight, time)) + ' kcal'
+        else:
+            print 'Bad choice'
